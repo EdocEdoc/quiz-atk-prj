36249c611c103966b9fc9b5eb524662a80f35c0a
va-gen-dev <vagenodipa@gmail.com>
2025-10-12 22:48:51 +0800

    feat: battle page and logic

--- web-app/src/utils/types.js
+++ web-app/src/utils/types.js
@@ -4,22 +4,23 @@ export const createUser = (uid, username, avatarUrl = null) => ({
   username,
   rating: 1000,
   avatarUrl,
-  createdAt: new Date()
+  createdAt: new Date(),
 });
 
 // Room data model
 export const createRoom = (hostId, hostTopic) => ({
   hostId,
   guestId: null,
-  status: 'waiting', // waiting, generating, lecture, battle, finished
+  status: "waiting", // waiting, generating, lecture, battle, finished
   hostTopic,
   guestTopic: null,
   finalTopic: null,
   lecture: null,
   quizList: [],
-  currentTurn: null,
+  currentTurn: "host",
+  currentAction: "attack", // attack or defend
   hp: { host: 10, guest: 10 },
-  createdAt: new Date()
+  createdAt: new Date(),
 });
 
 // Quiz question model
@@ -27,7 +28,7 @@ export const createQuizQuestion = (question, choices, answerIndex) => ({
   question,
   choices,
   answerIndex,
-  id: Math.random().toString(36).substr(2, 9)
+  id: Math.random().toString(36).substr(2, 9),
 });
 
 // Match log model
@@ -36,24 +37,24 @@ export const createMatch = (roomId, winnerId, events = []) => ({
   winnerId,
   events,
   duration: 0,
-  createdAt: new Date()
+  createdAt: new Date(),
 });
 
 // Game status constants
 export const GAME_STATUS = {
-  WAITING: 'waiting',
-  GENERATING: 'generating',
-  LECTURE: 'lecture',
-  BATTLE: 'battle',
-  FINISHED: 'finished'
+  WAITING: "waiting",
+  GENERATING: "generating",
+  LECTURE: "lecture",
+  BATTLE: "battle",
+  FINISHED: "finished",
 };
 
 // Battle events
 export const BATTLE_EVENTS = {
-  ATTACK: 'attack',
-  DEFEND: 'defend',
-  DAMAGE: 'damage',
-  BLOCK: 'block',
-  WIN: 'win',
-  LOSE: 'lose'
+  ATTACK: "attack",
+  DEFEND: "defend",
+  DAMAGE: "damage",
+  BLOCK: "block",
+  WIN: "win",
+  LOSE: "lose",
 };
